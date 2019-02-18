24e720cc78a6306f641217de47aeb4680c2310eb
IFilonov <filonovia@gmail.com>
2019-02-18 13:30:20 +0500

    add devise authentification

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'servers#index'
+  devise_for :users, path: :neptuns, path_names: {sign_in: :login, sing_out: :logout}
   resources :servers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
