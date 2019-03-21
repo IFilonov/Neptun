9182f7fe38411a8a81945bbd3de14ddb584bce86
IFilonov <filonovia@gmail.com>
2019-03-21 23:33:02 +0500

    Add views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,10 +5,52 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+  def new
+    @group = Group.new
+  end
+
+  def edit
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to @group, notice: 'Group was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def show
+  end
+
+  def update
+    respond_to do |format|
+      if @group.update(group_params)
+        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @group.destroy
+    respond_to do |format|
+      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
   def set_group
     @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :align_left)
   end
 end
