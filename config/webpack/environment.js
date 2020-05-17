a48af1e23d6102fadf00df03f0db6dd0106a84cc
Filonov Igor <filonovia@gmail.com>
2020-05-17 23:34:06 +0500

    bundle exec rails webpacker:install:vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 const erb = require('./loaders/erb')
 const jquery = require('./plugins/jquery')
 
 environment.plugins.prepend('jquery', jquery)
 environment.loaders.prepend('erb', erb)
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
